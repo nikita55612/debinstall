371a71ac87d8d40ddfd1e4e202a4aa073144fcbe
nlkli <groz.yt56@gmail.com>
2025-12-09 09:55:16 +0300

    fix gokapi

--- gokapi.sh
+++ gokapi.sh
@@ -27,5 +27,26 @@ chmod +x /opt/gokapi/gokapi
 
 port=$(inputport "Введите порт (1024-65535): ")
 
-/opt/gokapi/gokapi --port "$port" --install-service
-systemctl enable --now gokapi
+if ! systemctl list-unit-files --type=service | grep -q gokapi.service; then
+    cat > /etc/systemd/system/gokapi.service <<EOF
+[Unit]
+Description=Gokapi self-hosted file share
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/opt/gokapi
+ExecStart=/opt/gokapi/gokapi --port $port
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    systemctl enable --now gokapi
+fi
+
+systemctl restart gokapi || systemctl start gokapi
