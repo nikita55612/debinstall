d7c522b427273167885e3b95ed035dd6f49ee309
nlkli <groz.yt56@gmail.com>
2025-12-12 10:25:55 +0300

    vim config

--- vim.sh
+++ vim.sh
@@ -55,8 +55,8 @@ nnoremap <leader>p "+p
 nnoremap <leader>P "+P
 nnoremap <leader>q :quit<CR>
 nnoremap <leader>w :write<CR>
+nnoremap <leader>E :edit %:h<CR>
 nnoremap <leader>e :edit .<CR>
-nnoremap <leader>e :edit #<CR>
 nnoremap n nzzzv
 nnoremap N Nzzzv
 EOF
